96f935bfc4ef5f5dde9a7186fa3e699eced4d4ce
kdncode <kdncode@gmail.com>
2018-05-01 00:40:32 -0500

    Added UPDATE

--- routes/index.js
+++ routes/index.js
@@ -23,4 +23,27 @@ router.get('/delete/:deleteid', function(req, res, next) {
 
 });
 
+/* UPDATE data in View page. */
+router.get('/update/:updateid', function(req, res, next) {
+  var id2 = req.params.updateid;
+  contactModel.find({_id : id2}, function(err, data) {
+    res.render('update', { title : 'Update data', userData : data})
+  });
+});
+
+/* POST to update data in View page. */
+router.post('/update/:updateid', function(req, res, next) {
+  var id2 = req.params.updateid;
+
+  // Base on id, name, age then manipulate from View to DB
+  contactModel.findById(id2, function(err, data) {
+    if (err) return handleError(err);
+    data.name = req.body.name;
+    data.age = req.body.age;
+    data.save();
+    res.redirect('/view')
+  })
+  
+});
+
 module.exports = router;
